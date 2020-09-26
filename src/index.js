91fcb371dcc2a44316cc5eb090ab907de81435cd
Matthew Pagan <mastapegs01@gmail.com>
2020-09-25 22:15:55 -0400

    more coloring changes

--- src/index.js
+++ src/index.js
@@ -3,13 +3,11 @@ import './drill-app'
 
 render(html`
   <style>
-    * {
-      box-sizing: border-box;
-    }
     body {
       margin: 0;
       padding: 0;
       font: 16px/1.5 Arial, Helvetica, sans-serif;
+      background-color: hsla(0, 0%, 80%, 1);
     }
   </style>
   <drill-app></drill-app>
