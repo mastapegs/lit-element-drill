ba81937a161c6e80a85e6b1a5f6178d9b2010659
Matthew Pagan <mastapegs01@gmail.com>
2020-09-28 21:35:31 -0400

    adding router testing

--- src/web-components/app-router/app-router.js
+++ src/web-components/app-router/app-router.js
@@ -1,11 +1,31 @@
 import { LitElement, html } from 'lit-element'
 
 class AppRouter extends LitElement {
+
+  constructor() {
+    super()
+    this.setupRouter()
+  }
+
+  setupRouter() {
+    window.addEventListener('popstate', (e) => {
+      console.log(e)
+    })
+  }
+
   render() {
     return html`
-      <div>Test</div>
+      <div>
+        <p>Router in progress.</p>
+      </div>
+      <ul>
+        <li><a href='/'>Home</a></li>
+        <li><a href='/about'>About</a></li>
+        <li><a href='/contact'>Contact</a></li>
+      </ul>
     `
   }
+
 }
 
 customElements.define('app-router', AppRouter)
